e3dbfc93535aaf79c2d37f8e58b21ae74e52b97b
TarunBabbar <babbar.tarun@gmail.com>
2017-05-24 14:00:38 +0530

    Added Source Code

---
+++ MyFirstSeleniumWebApplication/GoogleSearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium;
+
+namespace MyFirstSeleniumWebApplication
+{
+    [TestClass]
+    public class GoogleSearch
+    {
+        [TestMethod]
+        public void VerifyGoogleSearch()
+        {
+            // Create web driver instance for Internet Explorer
+            IWebDriver webDriver;
+            InternetExplorerOptions opt = new InternetExplorerOptions
+            {
+                EnableNativeEvents = true,
+                IgnoreZoomLevel = true,
+                IntroduceInstabilityByIgnoringProtectedModeSettings = true,
+            };
+
+            webDriver = new InternetExplorerDriver(opt);
+            webDriver.Manage().Window.Maximize();
+
+            // Launch IE
+            webDriver.Navigate().GoToUrl("http://www.google.com");
+
+            // Identify Google Search TextBox:
+            IWebElement googleSearchTextBox = webDriver.FindElement(By.Id("lst-ib"));
+            googleSearchTextBox.SendKeys("https://medium.com/");
+            IWebElement googleSearchButton = webDriver.FindElement(By.Id("_fZl"));
+            googleSearchButton.Click();
+        }
+    }
+}
